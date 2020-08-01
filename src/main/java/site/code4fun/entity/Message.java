0d4ad77157acf8bde4c11d3bbe3f84fddae93ce4
TrungTQ <trungtrandb@gmail.com>
2020-08-01 17:40:08 +0700

    --ammend

--- src/main/java/site/code4fun/entity/Message.java
+++ src/main/java/site/code4fun/entity/Message.java
@@ -1,12 +1,36 @@
 package site.code4fun.entity;
 
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@Table(name = "tblMessage")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
-	private String to;
-    private String from;
-    private String text;
+	
+	@Column(name = "send_to")
+	protected String to;
+	
+	@Column(name = "send_from")
+	protected String from;
+	
+	protected String text;
+	
+	@Column(name = "created_date")
+	protected Timestamp createdDate;
+	
+	protected String status;
+	
+	
 }
